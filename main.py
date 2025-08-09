c66132b73348d7a8dcb7798ca0d8708945267423
noteblockai <noteharuhi@gmail.com>
2025-08-10 01:07:32 +0900

    Update main.py

--- main.py
+++ main.py
@@ -36,23 +36,24 @@ def get_note_block_base_block(instrument_name):
 def get_redstone_coordinates(instrument_name):
     """악기별로 레드스톤 블럭이 설치될 y, z 좌표 반환"""
     redstone_coords = {
-        "harp": (10, 132),
-        "bass": (10, 124),
-        "basedrum": (10, 116),
-        "snare": (10, 118),
-        "hat": (10, 120),
-        "guitar": (10, 130),
-        "flute": (10, 128),
-        "bell": (10, 142),
-        "chime": (10, 134),
-        "xylophone": (10, 136),
-        "iron_xylophone": (10, 140),
-        "cow_bell": (10, 122),
-        "didgeridoo": (10, 126),
-        "bit": (10, 138),
-        "banjo": (10, 114),
-        "pling": (10, 112)
+        "harp": (10, 21),
+        "bass": (10, 13),
+        "basedrum": (10, 5),
+        "snare": (10, 7),
+        "hat": (10, 9),
+        "guitar": (10, 19),
+        "flute": (10, 17),
+        "bell": (10, 31),
+        "chime": (10, 23),
+        "xylophone": (10, 25),
+        "iron_xylophone": (10, 29),
+        "cow_bell": (10, 11),
+        "didgeridoo": (10, 15),
+        "bit": (10, 27),
+        "banjo": (10, 3),
+        "pling": (10, 1)
     }
+
     return redstone_coords[instrument_name]  # (y, z) 튜플 반환
 
 def get_instrument_name(instrument_idx):
@@ -79,7 +80,7 @@ def get_instrument_name(instrument_idx):
 
 start_x = 30  # pitch 방향
 start_y = 4
-start_z = 0  # velocity 방향
+start_z = 1  # velocity 방향
 
 # 악기별로 개별 파일 생성
 for instrument_idx in range(16):
@@ -88,7 +89,7 @@ for instrument_idx in range(16):
     base_block = get_note_block_base_block(base_instrument)
     redstone_y, redstone_z = get_redstone_coordinates(base_instrument)  # 악기별 레드스톤 좌표
     
-    for velocity in range(100):      # z 방향 (velocity)
+    for velocity in range(150):      # z 방향 (velocity)
         for pitch_idx in range(73):  # x 방향 (pitch)
             x = start_x + pitch_idx
             y = start_y + (instrument_idx * 7)
@@ -102,14 +103,14 @@ for instrument_idx in range(16):
             else:
                 instrument = base_instrument
             
-            volume = round(velocity / 100, 2)
+            volume = round((velocity / 150) * 100 / 100, 2)
 
             playsound_cmd = f'execute as @p at @p run playsound minecraft:block.note_block.{instrument} master @p ~ ~ ~ {volume} {mc_pitch}'
             setblock_cmd = f'setblock {x} {y} {z} minecraft:command_block[facing=down]{{Command:"{playsound_cmd}"}}'
             chain_cmd = f'setblock {x} {y-1} {z} minecraft:chain_command_block[facing=down]{{Command:"setblock ~ ~2 ~ air",auto:1b}}'
             note_cmd = f'setblock {x} {y+2} {z} minecraft:{base_block}'
             # 추가 체인 커맨드 블럭 (악기별 임의의 위치에 레드스톤 블럭 설치)
-            additional_redstone_cmd = f'setblock {x} {y-2} {z} minecraft:chain_command_block[facing=down]{{Command:"setblock {pitch_idx+30} {redstone_y} {redstone_z} minecraft:redstone_block",auto:1b}}'
+            additional_redstone_cmd = f'setblock {x} {y-2} {z} minecraft:chain_command_block[facing=down]{{Command:"setblock {pitch_idx+120} {redstone_y} {redstone_z} minecraft:redstone_block",auto:1b}}'
 
             commands.extend([setblock_cmd, chain_cmd, additional_redstone_cmd, note_cmd])
     
